90647f7f863c3ea7891adc02bc3754a90fb4c4e5
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-12-10 21:02:18 +0100

    Update fun-saludar.js

--- plugins/fun-saludar.js
+++ plugins/fun-saludar.js
@@ -1,12 +1,12 @@
 // Función creada por Katashi Fukushima. Está a libre edición de videos para el envío del saludo
 
 let handler = async (m, { conn, usedPrefix, command}) => {
-let pp = ['https://qu.ax/Tvpv.mp4', 'https://qu.ax/Tvpv.mp4','https://qu.ax/Tvpv.mp4']
+let pp = ['https://tinyurl.com/294oahv9']
 //let pp2 = 'https://tinyurl.com/294oahv9'
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
-if (!who) throw '😸 *Etiqueta al usuario que quieres saludar.* 👋\nEjemplo:\n.saludar @GataBot'
+if (!who) throw '😸 *Etiqueta al usuario que quieres saludar.* 👋\nEjemplo:\n.saludar @MichiBot'
 let name2 = conn.getName(who)
 let name = conn.getName(m.sender)
 
